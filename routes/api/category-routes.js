bce1f50128f06395f5e24a6585e34ef6fbd334bc
Savannah-Shifflet <savannah.shifflet@gmail.com>
2023-08-07 20:33:40 -0500

    created GET routes for category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,14 +3,28 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+router.get('/', async (req, res) => {
+  // find all categories & its associated products
+  try{
+    const categoryData = await Category.findAll({
+      include: Product 
+    });
+    res.status(200).json(categoryData); 
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+router.get('/:id', async (req, res) => {
+  // find one category by its `id` value and its associated products
+  try{
+    const categoryId = await Category.findByPk(req.params.id, {
+      include: Product
+    });
+    res.status(200).json(categoryId);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
