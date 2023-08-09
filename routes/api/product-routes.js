a207e6d09d6bd4b1aeca8ea002e17e5be361a0dc
Savannah-Shifflet <savannah.shifflet@gmail.com>
2023-08-08 22:08:45 -0500

    created Product delete route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -18,8 +18,7 @@ router.get('/', async (req, res) => {
 
 // get one product
 router.get('/:id', async (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+  // find a single product by its `id` and include its associated Category and Tag data
   try{
     const productData = await Product.findByPk(req.params.id, {
       include: [{model: Category}, {model: Tag, through: ProductTag}]
@@ -32,17 +31,9 @@ router.get('/:id', async (req, res) => {
 
 // create new product
 router.post('/', (req, res) => {
-  /* req.body should look like this...
-    {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
-    }
-  */
   Product.create(req.body)
     .then((product) => {
-      // if there's product tags, we need to create pairings to bulk create in the ProductTag model
+      // if there's product tags, create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
         const productTagIdArr = req.body.tagIds.map((tag_id) => {
           return {
@@ -107,8 +98,16 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try{
+    const productData = await Product.destroy({
+      where: {id: req.params.id}
+    });
+    res.status(200).json(productData);
+  }catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
